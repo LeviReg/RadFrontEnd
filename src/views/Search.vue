101d1329fb174dfc1f06b8318f8129297e477b17
Levi Regan <S00108881@mail.itsligo.ie>
2020-05-18 13:21:36 +0100

    Fixed search function

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,28 +1,33 @@
 <template>
-  <div>
-    <b-field label="Search">
-      <b-input v-model="query" @change.native="onSearch"></b-input>
-    </b-field>
+  <div :class="{ 'modal-card': assignment }" style="width: auto">
+    <header v-if="assignment" class="modal-card-head">
+      <p class="modal-card-title">Add book to assignment</p>
+    </header>
 
-    <p v-if="books && books.length" class="m-b-md m-t-lg">
-      Found
-      <span class="has-text-primary has-text-weight-bold ">{{
-        books.length
-      }}</span>
-      results
-    </p>
-    <div class="columns is-multiline">
-      <template v-if="books && books.length">
-        <div v-for="book in books" :key="book.etag" class="column is-3">
-          <Book :book="book" />
+    <div :class="{ 'modal-card-body': assignment }">
+      <b-field label="Search">
+        <b-input v-model="query" @change.native="onSearch"></b-input>
+      </b-field>
+      <p v-if="books && books.length" class="m-b-md m-t-lg">
+        Found
+        <span class="has-text-primary has-text-weight-bold ">{{
+          books.length
+        }}</span>
+        results
+      </p>
+      <div class="columns is-multiline">
+        <template v-if="books && books.length">
+          <div v-for="book in books" :key="book.etag" class="column is-4">
+            <Book :book="book" :assignment="assignment" @bookAdded="addBook" />
+          </div>
+        </template>
+        <div
+          v-else
+          class="column is-centered has-text-centered is-size-4 m-t-lg has-text-grey"
+        >
+          <b-icon size="is-large" icon="magnify"></b-icon>
+          <p class="m-l-md">Search...</p>
         </div>
-      </template>
-      <div
-        v-else
-        class="column is-centered has-text-centered is-size-4 m-t-lg has-text-grey"
-      >
-        <b-icon size="is-large" icon="magnify"></b-icon>
-        <p class="m-l-md">Search...</p>
       </div>
     </div>
   </div>
@@ -36,6 +41,7 @@ export default {
   components: {
     Book
   },
+  props: ['assignment'],
   data() {
     return {
       books: [],
@@ -50,6 +56,9 @@ export default {
           this.books = response.data.items;
         })
         .catch(err => console.log(err));
+    },
+    addBook(item) {
+      this.$emit('addToAssignment', item);
     }
   }
 };
